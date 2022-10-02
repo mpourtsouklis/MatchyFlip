6134db4cef900a3614df521a19d727325538ae70
Tasos Mpourtsouklis <103905458+tasosmpourtsouklis@users.noreply.github.com>
2022-10-03 02:45:08 +0300

    Implemented card's class.

---
+++ card.py
@@ -0,0 +1,8 @@
+class card:
+    # Constructor
+    def __init__(self, symbol, series, desc, worth, cond):
+        self.symbol = symbol
+        self.series = series
+        self.desc = desc
+        self.worth = worth
+        self.cond = cond
